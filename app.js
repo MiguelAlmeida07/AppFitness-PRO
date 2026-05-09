edb9af8f825030b7ede68420ffb5782e595d68fe
Miguel Almeida <luismiguelalmeida0784@gmail.com>
2026-05-09 16:40:11 +0100

    Add Bolo de Banana recipe (r7) to database

--- app.js
+++ app.js
@@ -90,6 +90,10 @@ const DB = {
     {id:'r6',name:'Salada Atum & Quinoa',emoji:'🥗',cat:'Almoço',time:'15min',cal:380,prot:35,carb:38,fat:9,
       ing:[{fid:'f07',g:140},{fid:'f22',g:150},{fid:'f23',g:80},{fid:'f13',g:40},{fid:'f10',g:8}],
       steps:['Coze a quinoa 12min.','Escorre o atum.','Mistura tudo numa taça.','Tempera com limão, sal e azeite.','Serve frio.'],mid:'m3'},
+    {id:'r7',name:'Bolo de Banana',emoji:'🍌',cat:'Lanche',time:'40 min',cal:842,prot:56,carb:116,fat:20,
+      ing:[{fid:'f06',g:200},{fid:'f04',g:100},{fid:'f05',g:100},{fid:'f20',g:30}],
+      steps:['Coloca as 2 bananas, 2 ovos e 100g de farinha de aveia no liquidificador e tritura tudo.','Adiciona 1 colher de chá de fermento em pó e volta a misturar.','Unta com azeite uma forma e coloca lá o preparado.','Leva ao forno a 180º por 30 a 40 minutos (vê com um palito se está cozido).','Para a cobertura coloca 30g whey numa taça e adiciona um pouco de água, mistura bem até ficar com uma consistência tipo mousse (cuidado para não colocares demasiada água).'],
+      mid:'m4'},
   ],
 };
 
